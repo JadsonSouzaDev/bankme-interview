b8f990fd2299cd75e1ea5576ed459c2c70a076be
Jadson Souza <84452479+JadsonSouzaDev@users.noreply.github.com>
2025-07-02 21:12:03 -0300

    refactor(shared): :bug: change to datestring

--- packages/shared/src/dtos/payables/update-payable.dto.ts
+++ packages/shared/src/dtos/payables/update-payable.dto.ts
@@ -1,19 +1,26 @@
-import { IsNotEmpty, IsUUID, IsNumber, Min, IsOptional, IsDate } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsUUID,
+  IsNumber,
+  Min,
+  IsOptional,
+  IsDateString,
+} from "class-validator";
 
 export class UpdatePayableDto {
-  @IsNotEmpty({ message: 'O valor é obrigatório.' })
-  @IsNumber({}, { message: 'O valor deve ser um número.' })
-  @Min(0.01, { message: 'O valor deve ser maior que 0.' })
+  @IsNotEmpty({ message: "O valor é obrigatório." })
+  @IsNumber({}, { message: "O valor deve ser um número." })
+  @Min(0.01, { message: "O valor deve ser maior que 0." })
   @IsOptional()
   value?: number;
 
-  @IsNotEmpty({ message: 'A data de emissão é obrigatória.' })
-  @IsDate({ message: 'A data de emissão deve ser uma data válida.' })
+  @IsNotEmpty({ message: "A data de emissão é obrigatória." })
+  @IsDateString({}, { message: "A data de emissão deve ser uma data válida." })
   @IsOptional()
-  emissionDate?: Date;
+  emissionDate?: string;
 
-  @IsNotEmpty({ message: 'O assignor é obrigatório.' })
-  @IsUUID('4', { message: 'O assignor deve ser um UUID válido.' })
+  @IsNotEmpty({ message: "O assignor é obrigatório." })
+  @IsUUID("4", { message: "O assignor deve ser um UUID válido." })
   @IsOptional()
   assignor?: string;
 }
