048df315bbdffa9ffd32ce5c75075745da30096c
Jadson Souza <84452479+JadsonSouzaDev@users.noreply.github.com>
2025-07-03 16:15:14 -0300

    chore(backend): :art: mock a email service call

--- apps/backend/src/core/common/application/events/event-bus.impl.ts
+++ apps/backend/src/core/common/application/events/event-bus.impl.ts
@@ -1,3 +1,5 @@
+import { BatchCompletedEvent } from '../../../payables/domain/events/batch-completed.event';
+import { PayableErrorEvent } from '../../../payables/domain/events/payable-error.event';
 import { DomainEvent } from '../../domain/domain-event';
 import { EventBus } from './event-bus.interface';
 import { Logger } from '@nestjs/common';
@@ -7,17 +9,46 @@ export class EventBusImpl implements EventBus {
   async publish(event: DomainEvent): Promise<void> {
     this.logger.verbose(event.type);
 
-    // Aqui você pode implementar a lógica real de publicação
-    // Por exemplo, enviar para um message broker como RabbitMQ, Kafka, etc.
-    // Ou salvar em uma tabela de eventos para processamento posterior
+    //TODO: Move to a consumer
+    if (event.type === 'BatchCompleted') {
+      await this.sendEmailBatchCompleted(event as BatchCompletedEvent);
+    }
 
-    // Simulação de processamento assíncrono
-    await new Promise((resolve) => setTimeout(resolve, 100));
+    //TODO: Move to a consumer
+    if (event.type === 'PayableError') {
+      await this.sendEmailPayableFailed(event as PayableErrorEvent);
+    }
   }
 
   async publishAll(events: DomainEvent[]): Promise<void> {
     for (const event of events) {
       await this.publish(event);
     }
+  }
+
+  private async sendEmailBatchCompleted(
+    event: BatchCompletedEvent,
+  ): Promise<void> {
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        this.logger.verbose(
+          `Sending email batch completed: BatchId: ${event.payload.batchId} SuccessCount: ${event.payload.successCount} FailedCount: ${event.payload.failedCount}`,
+        );
+        resolve(true);
+      }, 100),
+    );
+  }
+
+  private async sendEmailPayableFailed(
+    event: PayableErrorEvent,
+  ): Promise<void> {
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        this.logger.verbose(
+          `Sending email payable failed: ${JSON.stringify(event.payload)}`,
+        );
+        resolve(true);
+      }, 100),
+    );
   }
 }
